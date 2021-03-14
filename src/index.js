5272fd6951ddfdb7b91e9fb71bfb1fecb2b2bda0
Katala121 <sanyafootb@list.ru>
2021-03-14 04:06:26 +0300

    feat: implement fanction

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!arguments.length || matrix.length == 0) return []
+  return matrix.reduce((arr, item, i) => {
+    if (i % 2 != 0) arr = arr.concat(...item.reverse());
+    else arr = arr.concat(...item);
+    return arr
+  }, [])
 }
